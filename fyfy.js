36ad940d8cec19d27b2b4608b6903e6b50c82e5f
Sarah <sarah.stichel@skola.taby.se>
2024-03-27 16:24:02 +0100

    aaa

--- fyfy.js
+++ fyfy.js
@@ -1,1 +1,6 @@
 //massa kooood
+// requestAnimationFrame(func) anropa först i kod och sen i själva ritfunction
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+ctx.fillstyle = "green";
+ctx.fillrect(0, 0, canvas.height, canvas.width);
