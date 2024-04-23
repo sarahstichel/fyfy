69c57aafb4d271477eca9f9a6014e6f97adad38e
Taras Pustovit <pustovittaras48@gmail.com>
2024-04-23 17:06:25 +0200

    SpriteSheets FUNKAR ÄNTLIGEN

--- Test.js
+++ Test.js
@@ -1,56 +1,51 @@
-let playerState = "idle";
+let playerState = "jump";
 
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = (canvas.width = 600);
 const CANVAS_HEIGHT = (canvas.height = 600);
 
-let gameFrame = 0;
-const staggerFrames = 5;
-const spriteAnimations = [];
-const playerIdleImg = new Image();
-playerIdleImg.src = "Player1_Idle.png";
-const playerJumpImg = new Image();
-playerJumpImg.src = "Player1_Jump.png";
-const animationStates = {
-  idle: {
-    name: "idle",
-    image: playerIdleImg,
-    frames: 10,
-  },
-  jump: {
-    name: "jump",
-    image: playerJumpImg,
-    frames: 3,
-  },
-};
-
+const playerImage = new Image();
+playerImage.src = "Player1_Combined.png";
 const spriteWidth = 120;
 const spriteHeight = 80;
 
-for (const state in animationStates) {
+let gameFrame = 0;
+const staggerFrames = 5;
+const spriteAnimations = [];
+const animationStates = [
+  {
+    name: "idle",
+    frames: 10,
+  },
+  {
+    name: "death",
+    frames: 10,
+  },
+  {
+    name: "run",
+    frames: 10,
+  },
+  {
+    name: "attack",
+    frames: 4,
+  },
+  {
+    name: "jump",
+    frames: 3,
+  },
+];
+animationStates.forEach((state, index) => {
   let frames = {
     loc: [],
   };
-  let index = 0;
-  for (let j = 0; j < animationStates[state].frames; j++) {
+  for (let j = 0; j < state.frames; j++) {
     let positionX = j * spriteWidth;
     let positionY = index * spriteHeight;
     frames.loc.push({ x: positionX, y: positionY });
   }
-  spriteAnimations[animationStates[state].name] = frames;
-}
-// animationStates.forEach((state, index) => {
-//   let frames = {
-//     loc: [],
-//   };
-//   for (let j = 0; j < state.frames; j++) {
-//     let positionX = j * spriteWidth;
-//     let positionY = index * spriteHeight;
-//     frames.loc.push({ x: positionX, y: positionY });
-//   }
-//   spriteAnimations[state.name] = frames;
-// });
+  spriteAnimations[state.name] = frames;
+});
 console.log(spriteAnimations);
 
 function animate() {
@@ -62,7 +57,7 @@ function animate() {
   let frameY = spriteAnimations[playerState].loc[position].y;
 
   ctx.drawImage(
-    animationStates[playerState].image,
+    playerImage,
     frameX,
     frameY,
     spriteWidth,
--- fyfy.js
+++ fyfy.js
@@ -7,20 +7,25 @@ const ctx = canvas.getContext("2d");
 const backgroundImage = new Image(); //Skapar en ny variabel som är bild
 backgroundImage.src = "theme.png"; //Bilden source är theme.png
 //Skapar Player elemenst
+Player1Img = new Image();
+Player1Img.src = "Player1_combined.png";
+Player2Img = new Image();
+Player2Img.src = "Player2_combined.png";
 class Player {
-  constructor(color, xPos, yPos, num) {
+  constructor(xPos, yPos, num, image) {
     this.name = "";
     this.hp = 100;
     this.strength = 3;
     this.medkit = 0;
     this.strengthPotion = 0;
-    this.color = color;
     this.x = xPos;
     this.y = yPos;
     this.speed = 10;
     this.yspeed = 0;
     this.xspeed = 0;
     this.number = num;
+    this.playerImage = image;
+    this.playerState = "idle";
   }
   useMedkit() {
     if (this.medkit > 0) {
@@ -28,9 +33,26 @@ class Player {
       this.medkit -= 1;
     }
   }
-  draw(ctx) {
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this.y, 25, 25);
+  animate(ctx) {
+    let position =
+      Math.floor(gameFrame / staggerFrames) %
+      spriteAnimations[this.playerState].loc.length;
+    let FrameX = spriteAnimations[this.playerState].loc[position].x;
+    let FrameY = spriteAnimations[this.playerState].loc[position].y;
+
+    ctx.drawImage(
+      this.playerImage,
+      FrameX,
+      FrameY,
+      spriteWidth,
+      spriteHeight,
+      this.x,
+      this.y,
+      spriteWidth * 3,
+      spriteHeight * 3
+    );
+
+    gameFrame++;
   }
   useStrengthpotion() {
     if (this.strengthPotion > 0) {
@@ -44,14 +66,55 @@ class Player {
   }
 } //Här slutar player-klassen
 
+const spriteWidth = 120;
+const spriteHeight = 80;
+
+let gameFrame = 0;
+const staggerFrames = 5;
+const spriteAnimations = [];
+const animationStates = [
+  {
+    name: "idle",
+    frames: 10,
+  },
+  {
+    name: "death",
+    frames: 10,
+  },
+  {
+    name: "run",
+    frames: 10,
+  },
+  {
+    name: "attack",
+    frames: 4,
+  },
+  {
+    name: "jump",
+    frames: 3,
+  },
+];
+animationStates.forEach((state, index) => {
+  let frames = {
+    loc: [],
+  };
+  for (let j = 0; j < state.frames; j++) {
+    let positionX = j * spriteWidth;
+    let positionY = index * spriteHeight;
+    frames.loc.push({ x: positionX, y: positionY });
+  }
+  spriteAnimations[state.name] = frames;
+});
+console.log(spriteAnimations);
+
 //Här börjar Sprite
 
 //Defenition av medkit och strength potion
 const medkitPower = 20;
 const strengthPotionPower = 5;
 //Spelare 1
-const Player1 = new Player("black", 100, 400, 1);
-const Player2 = new Player("blue", 1100, 400, 2);
+const Player1 = new Player(100, 400, 1, Player1Img);
+const Player2 = new Player(1100, 400, 2, Player2Img);
 
 class Platform {
   // draw(ctx) {
@@ -68,6 +131,7 @@ window.addEventListener("keydown", function (event) {
   switch (event.key) {
     case "w":
       Player1.yspeed = -Player1.speed;
+      Player1.playerState = "jump";
       // Player1.Sprite.Jump(ctx);
       break;
     case "s":
@@ -78,6 +142,7 @@ window.addEventListener("keydown", function (event) {
       break;
     case "d":
       Player1.xspeed = Player1.speed;
+      Player1.playerState = "run";
       break;
     case "ArrowUp":
       Player2.yspeed = -Player2.speed;
@@ -137,9 +202,8 @@ function animate(timestamp) {
   ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height); // Rita bakgrunden
   ctx.fillStyle = "black";
   ctx.fillRect(200, 200, 30, 200);
-  Player1.draw(ctx);
-  Player2.draw(ctx);
-
+  Player1.animate(ctx);
+  Player2.animate(ctx);
   if (Player1.y < canvas.height && Player1.x < canvas.width) {
     Player1.y += Player1.yspeed;
     Player1.x += Player1.xspeed;
@@ -154,7 +218,6 @@ function animate(timestamp) {
     Player2.yspeed = 0;
     Player2.xspeed = 0;
   }
-
   window.requestAnimationFrame(animate);
 }
 
